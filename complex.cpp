86818fbb92e9c000fb9ac4f588ed0fe8ee9836b6
Adams Twist <1275963@gmail.com>
2013-10-13 13:01:15 +0800

    Result - Sorry I am just not so familiar with the GIT

--- complex.cpp
+++ complex.cpp
@@ -1,9 +1,58 @@
-#include<iostream>
-using namespace std;
+#include "complex.h"
 
 void complex::show()
 {
-        cout<< real << "+" << com << "i" << endl;
+        cout<<real<<"+i*"<<com<<endl;
 }
 
+complex::complex(int a, int b)
+{
+        real=a;
+        com=b;
+}
 
+complex complex::operator + (complex c1)
+{
+        complex c;
+        c.real=c1.real+real;
+        c.com=c1.com+com;
+        return c;
+}
+
+complex complex :: operator - (complex c1)
+{
+        complex c;
+        c1.real=-c1.real;
+        c1.com=-c1.com;
+        c=c1+(*this);
+        return c;
+}
+
+complex complex :: operator * (complex c1)
+{
+        complex c;
+        c.real = ((this->real)*c1.real)-((this->com)*c1.com);
+        c.com = ((this->real)*c1.com)+((this->com)*c1.real);
+        return c;
+}
+
+complex complex :: operator / (complex c1)
+{
+        complex a1;
+        if(c1.real==0 && c1.com==0){
+                cout<< " WRONG METHOD "<<endl;
+                return a1;
+        }
+        else
+        {
+        a1.real = c1.real;
+        a1.com = - c1.com;
+        complex a2;
+        a2 = a1 * c1;
+        complex a3;
+        a3= (*this) * a1;
+        a3.real = a3.real / a2.real;
+        a3.com = a3.com / a2.real;
+        return a3;
+        }
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "complex.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        complex tryer(-3,-2);
+        complex tyer(0,0);
+        complex t;
+        t=tryer/tyer;
+        t.show();
+        return 0;
+}
